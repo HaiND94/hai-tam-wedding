9f5f2175b846429ace7b727177bc841702c00082
HaiND <haind@bfast.dev>
2026-07-16 11:26:21 +0700

    update edit port

--- server.py
+++ server.py
@@ -15,7 +15,7 @@ import os
 from datetime import datetime
 from http.server import SimpleHTTPRequestHandler, ThreadingHTTPServer
 
-PORT = 80
+PORT = 3000
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 DATA_DIR = os.path.join(BASE_DIR, 'data')
 CSV_PATH = os.path.join(DATA_DIR, 'wishes.csv')
